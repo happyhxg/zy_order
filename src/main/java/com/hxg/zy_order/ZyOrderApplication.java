75f4de01621feb580efeb0424fca552af1685566
hxg <292707429@qq.com>
2019-10-09 18:54:43 +0800

    项目初始化

---
+++ src/main/java/com/hxg/zy_order/ZyOrderApplication.java
@@ -0,0 +1,13 @@
+package com.hxg.zy_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyOrderApplication.class, args);
+    }
+
+}
